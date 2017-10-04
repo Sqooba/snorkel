1b60ae30ed0c7d7469e1e82c0457e3b41ebd955f
Adriano Pagano <adriano@sqooba.io>
2017-10-04 17:27:47 +0200

    ✨  Set default intepreter memory to half the available ram

--- env.sh
+++ env.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+MEMORY=8
+
+OS="$(uname -s)"
+if [[ $OS == 'Linux' ]]; then
+   TOTAL_MEMORY=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
+   MEMORY=$(($TOTAL_MEMORY/1048576))
+   CPUS=$(nproc --all)
+elif [[ $OS == 'Darwin' ]]; then
+   TOTAL_MEMORY=$(sysctl -n hw.memsize)
+   MEMORY=$(($TOTAL_MEMORY/1073741824))
+   CPUS=$(sysctl -n hw.ncpu)
+fi
+
+INTEPRETER_MEMORY=$((MEMORY/2))
+ZEPPELIN_INTP_MEMORY=${ZEPPELIN_INTP_MEMORY:--Xmx${INTEPRETER_MEMORY}g}
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export ZEPPELIN_ROOT_DIR=${ZEPPELIN_ROOT_DIR:-$SCRIPT_DIR/zeppelin}
 
