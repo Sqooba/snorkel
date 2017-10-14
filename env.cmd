583ca4e6a702ced605de6e986d5124da7a0a20a3
Lucy Linder <lucy.derlin@gmail.com>
2017-10-14 09:52:06 +0200

    set interpreter memory to half the size of the total RAM

--- env.cmd
+++ env.cmd
@@ -1,9 +1,17 @@
+@echo off
+
 pushd %~dp0
 set SCRIPT_DIR=%CD%
 popd
 
-set ZEPPELIN_INTP_MEMORY=4
-set ZEPPELIN_ROOT_DIR=%SCRIPT_DIR%\zeppelin
+:: find available RAM
+for /f "skip=1" %%p in ('wmic os get freephysicalmemory') do (
+  set /a memory=%%p/1048576
+  goto :done
+)
+:done
 
+set /a ZEPPELIN_INTP_MEMORY=%memory%/2
+set ZEPPELIN_ROOT_DIR=%SCRIPT_DIR%\zeppelin
 set ZEPPELIN_PORT=8080
 set SPARK_UI_PORT=4080
